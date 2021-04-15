1e435ccfa9a424c4a503b83d3b674b90520c70db
kbarne12 <kbarne12@gmail.com>
2021-04-15 18:25:41 -0400

    court-model

--- models/Review.js
+++ models/Review.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 const reviewSchema = new Schema({
     reviewer: {type: String, required: true},
     rating: {type: Number, min: 1, max: 5},
