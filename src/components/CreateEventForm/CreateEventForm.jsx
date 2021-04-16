e5b07c9bc86a5f0295b92d621d16d181e0ff8e24
mmpacker <mmpacker@gmail.com>
2021-04-15 22:00:44 -0400

    add LoginForm and CreateEventForm components

---
+++ src/components/CreateEventForm/CreateEventForm.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import authService from '../../services/authService';
+
+class CreateEventForm extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default CreateEventForm;
