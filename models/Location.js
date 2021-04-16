c4cebd3ece295ecf183cfe332e72d31441ada9e8
mmpacker <mmpacker@gmail.com>
2021-04-16 14:54:39 -0400

    update Review model to embed

--- models/Location.js
+++ models/Location.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const reviewSchema = new Schema({
+    reviewer: { type: Schema.Types.ObjectId, ref: 'User'},
+    rating: {type: Number, min: 1, max: 5},
+    content: {type: String}
+}, {
+    timestamps: true
+});
 
 const locationScema = new Schema({
     name: { 
