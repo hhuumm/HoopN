b4835a5650acd7e77a0a634608b81a926f8df18b
mmpacker <mmpacker@gmail.com>
2021-04-15 22:11:25 -0400

    add EventListCard component

---
+++ src/components/EventListCard/EventListCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function EventListCard({} {
+    return(
+        <>
+        </>
+    )
+})
+
+export default EventListCard;
