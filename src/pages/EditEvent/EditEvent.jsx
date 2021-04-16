5e64ea7e5f231d782e5663c6bbf4b924f55b251e
mmpacker <mmpacker@gmail.com>
2021-04-15 21:37:47 -0400

    add EditEvent page/css

---
+++ src/pages/EditEvent/EditEvent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './EditEvent.css'
+
+class EditEvent extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default EditEvent;
