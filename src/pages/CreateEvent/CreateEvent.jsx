da6d28d97309527c041f8e7f77b8f2f9df5cce50
mmpacker <mmpacker@gmail.com>
2021-04-15 21:35:30 -0400

    add CreateEvent page/css

---
+++ src/pages/CreateEvent/CreateEvent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './CreateEvent.css'
+
+class CreateEvent extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default CreateEvent;
