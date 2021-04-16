07b8c49e85ad2cb62ff7c5563a16b52fa4abf4a0
mmpacker <mmpacker@gmail.com>
2021-04-16 10:34:40 -0400

    update EventList page

--- src/pages/EventList/EventList.jsx
+++ src/pages/EventList/EventList.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './EventList.css'
+import EventListCard from '../../components/EventListCard/EventListCard'
 
 function EventList(props) {
     return (
         <>
+            <div className='EventList-grid'>
+                {props.events.map(event =>
+                    <EventListCard 
+                        key={event._id}
+                        event={event}
+                        user={props.user}
+                    />
+                )}
+            </div>
         </>
     )
 }
