587e8bb13a5a3201afdcb82c5d1b44bbe38ebefc
hhuumm <h4mid@Hotmail.com>
2021-04-17 10:24:21 -0400

    Fixing bug

--- src/components/EventDetailsCard/EventDetailsCard.jsx
+++ src/components/EventDetailsCard/EventDetailsCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function EventDetailsCard({} {
+function EventDetailsCard(props) {
     return(
         <>
         </>
     )
-})
+}
 
 export default EventDetailsCard;
