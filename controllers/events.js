e273313f7093497ff9ffe08623b91f929f2dab54
hhuumm <h4mid@Hotmail.com>
2021-04-18 11:47:05 -0400

    Routers for my games

--- controllers/events.js
+++ controllers/events.js
@@ -1,5 +1,4 @@
 const Event = require('../models/Event')
-// const { default: EventList } = require('../src/pages/EventList/EventList')
 
 module.exports = {
   create,
@@ -23,17 +22,17 @@ function deleteEvent(req, res) {
 function create(req, res) {
     req.body.createdBy = req.user._id
     req.body.participant = req.user._id
-    // req.body.location = req.location._id
+    req.body.location = req.location._id
     Event.create(req.body)
     .then(event => {res.json(event)})
     .catch(err => {res.json(err)})
   }
 
 function index(req, res) {
-  Event.find({})
-  .populate('createdBy')
-  .populate('location')
-  .populate('participant')
-  .then(events => {res.json(events)})
-  .catch(err => {res.json(err)})
+    Event.find({})
+    .populate('createdBy')
+    .populate('location')
+    .populate('participant')
+    .then(events => {res.json(events)})
+    .catch(err => {res.json(err)})
   }
