c53fd533b8b317f70c855559ac8b091679ef3e8a
mmpacker <mmpacker@gmail.com>
2021-04-18 09:55:09 -0400

    update Location model to embed Court model

--- models/Location.js
+++ models/Location.js
@@ -5,16 +5,19 @@ const reviewSchema = new Schema({
     reviewer: { type: Schema.Types.ObjectId, ref: 'User'},
     rating: {type: Number, min: 1, max: 5},
     content: {type: String}
-}, {
-    timestamps: true
-});
+}, {timestamps: true});
+
+const courtSchema = new Schema({
+    name: {type: String, required: true},
+    courtType: {type: String, default: 'Basketball'}
+}, {timestamps: true});
 
 const locationSchema = new Schema({
     name: {type: String, required: true},
     lat: {type: Number, required: true},
     lng: {type: Number, required: true},
-    court: {type: String, required: true}, 
+    courts: [courtSchema], 
     reviews: [reviewSchema]
-})
+}, {timestamps: true})
 
 module.exports = mongoose.model('Location', locationSchema);
