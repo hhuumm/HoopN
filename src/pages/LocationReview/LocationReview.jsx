13683fc8aeff73a912d6b24456c0d7855debb5c1
mmpacker <mmpacker@gmail.com>
2021-04-19 18:01:01 -0400

    stub location review page

--- src/pages/LocationReview/LocationReview.jsx
+++ src/pages/LocationReview/LocationReview.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import user from '../../../models/user';
 import './LocationReview.css'
 
 class LocationReview extends Component {
@@ -27,15 +28,17 @@ class LocationReview extends Component {
     }
 
     render() {
+        
+        // if(!location.reviews.some(u => {return u.reviewer === user._id}))
+        
         return (
             <div className="LocationReview">
 
                 <h1>Location Name</h1>
                 <h2>Location Address</h2>
                 <h2>Review Form:</h2> 
-                {/* Only show form if the user has not reviewed this location before. */}
-                if (!location.reviews.some(u =))
-                <form
+                
+                {/* <form
                     className="col-s12"
                     ref={this.formRef}
                     onSubmit={this.handleSubmit}
@@ -57,58 +60,28 @@ class LocationReview extends Component {
                     <div className="row">
                         <div className="input-field col s12">
                             <input
-                                name="court"
-                                placeholder="Court Name"
-                                id="court"
+                                name="content"
+                                placeholder="Write Review Here"
+                                id="location_review_content"
                                 type="text"
                                 className="active"
-                                value={this.state.formData.court.name} //? Add the name of the court here, if it is a new court name for the location, add the new court name to the database
+                                value={this.state.formData.content}
                                 onChange={this.handleChange}
                             />
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input
-                                name="date"
-                                // placeholder="Court Name"
-                                id="date"
-                                max="2031-04-17"
-                                type="date"
-                                className="active"
-                                value={this.state.formData.court.name} //? Add the name of the court here, if it is a new court name for the location, add the new court name to the database
-                                onChange={this.handleChange}
-                            >
-                            </input>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input
-                                name="time"
-                                id="time"
-                                // step="900"
-                                type="time"
-                                className="active"
-                                value={this.state.formData.court.time} //? Add the name of the court here, if it is a new court name for the location, add the new court name to the database
-                                onChange={this.handleChange}
-                            >
-                            </input>
-                        </div>
-                    </div>
-
                     <button
                         type="submit"
                         className="button"
                         disabled={this.state.invalidForm}
                     >
-                        <i className="material-icons left">Add Event</i>
+                        <i className="material-icons left">Add Review</i>
                     </button>
-                </form>
+                </form> */}
             </div>
         );
     }
 }
 
 
-export default CreateEvent;
+export default LocationReview;
