54938a94e79e3c61507c24e13ecda0723d6fd336
davidisurf <davidisurf@hotmail.com>
2021-04-19 13:33:44 -0400

    fix details card page

--- src/components/EventDetailsCard/EventDetailsCard.jsx
+++ src/components/EventDetailsCard/EventDetailsCard.jsx
@@ -7,10 +7,39 @@ import EventDetails from '../../pages/EventDetails/EventDetails' //??????
 
 function EventDetailsCard(props) {
 	//{user, event, court, handleDeleteEvent}
-	// const { user, event, deleteEvent, court} = props
-	console.log("Hello?")
+	const { user, event, deleteEvent, court} = props
 	return (
 		<>
+			<div className='EventList-grid'>
+				<Card style={{ width: '18rem' }}>
+					{/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+					<Card.Body>
+						<Card.Title>{event.title}</Card.Title>
+						<span>{event.date}</span><br />
+						<span>{event.time}</span><br />
+						<span>{event.createdBy.name}</span><br />
+						{/* <NavLink className="test" to="/events/details"><button href="/event/details" className="button" >View Details</button></NavLink> */}
+					</Card.Body>
+					{user && (user._id === event.createdBy._id) &&
+						<>
+							<div className="up-del" >
+								<Button
+									variant="primary"
+									type="submit"
+									onClick={() => deleteEvent(event._id)}
+								>Delete</Button>
+
+								<Link
+									to={{
+										pathname: '/edit',
+										state: { event }
+									}}
+								><Button variant="danger">Edit</Button></Link>
+							</div>
+						</>
+					}
+				</Card>
+			</div>
 		</>
 	)
 }
