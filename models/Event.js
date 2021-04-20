c2d180e89772c06c54281c907ea25bca7048345c
mmpacker <mmpacker@gmail.com>
2021-04-20 18:36:31 -0400

    Update models, update event list page

--- models/Event.js
+++ models/Event.js
@@ -1,20 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const reviewSchema = new Schema({
+
+    reviewer: { type: Schema.Types.ObjectId, ref: 'User'},
+    rating: {type: Number, min: 1, max: 5},
+    content: {type: String}
+    
+}, {timestamps: true});
+
 const eventSchema = new Schema ({
-    title: {
-        type: String, required: true
-    },
-    placeId: { type: String, required: true},
-    court: { type: String, required: true},
+
+    title: { type: String, required: true },
+    placeId: { type: String, required: true },
+    court: { type: String, required: true },
     createdBy: { type: Schema.Types.ObjectId, ref: 'User'},
     participant: [{ type: Schema.Types.ObjectId, ref: 'User'}],
     date: {type: String, format: Date, required: true},
     time: {type: String, format: Date, required: true},
+    reviews: [reviewSchema]
 
 }, { timestamps: true })
 
-module.exports =mongoose.model('Event', eventSchema)
-
-// time: {"type": "string", "format": "time"}
-// location: { type: Schema.Types.ObjectId, ref: 'Location'},
+module.exports = mongoose.model('Event', eventSchema)
--- src/pages/EventList/EventList.jsx
+++ src/pages/EventList/EventList.jsx
@@ -2,65 +2,83 @@ import React from 'react';
 import EventListCard from '../../components/EventListCard/EventListCard'
 
 function EventList(props) {
-    const { user, events,handleShow,history } = props
+    const { user, events, handleShow, history, places } = props
     const myGames = []
+    const gamesNearMe = []
 
-    if (props.myEvent) {
+    if (places) {
+        if (props.myEvent) {
 
-        console.log("This is my events\n", user._id, "\n^^This is the user id\n", events)
-        events.forEach(ev => {
-            ev.participant.forEach(player => {
-                if (player._id == user._id) {
-                    myGames.push(ev)
-                }
+            console.log("This is my events\n", user._id, "\n^^This is the user id\n", events)
+            events.forEach(ev => {
+                ev.participant.forEach(player => {
+                    if (player._id == user._id) {
+                        myGames.push(ev)
+                    }
 
+                })
             })
-        })
 
+            return (
+                <>
+                    {myGames.length ?
+                        <div className='EventList-grid'>
+                            {myGames.map(event =>
+                                <EventListCard
+                                    event={event}
+                                    user={user}
+                                    handleShow={handleShow}
+                                    history={history}
+                                />
+                            )
+                            }
+                        </div>
+                        :
+                        <div>
+                            <img src="https://media.giphy.com/media/vZROLXfaqhbhHO8qwr/giphy.gif"></img>
+                        </div>
+                    }
+                </>
+            )
+        }
+        else {
+            console.log("This is all events")
+            
+            events.forEach(ev => {
+                places.forEach(place => {
+                    if(place.place_id === ev.placeId) {
+                        gamesNearMe.push(ev)
+                    }
+                })
+            })
+            
+            return (
+                <>
+                    {gamesNearMe.length ?
+                        <div className='EventList-grid'>
+                            {gamesNearMe.map(event =>
+                                <EventListCard
+                                    key={event._id}
+                                    event={event}
+                                    user={props.user}
+                                    handleShow={handleShow}
+                                    history={history}
+                                />
+                            )}
+                        </div>
+                        :
+                        <div>
+                            <img src="https://media.giphy.com/media/vZROLXfaqhbhHO8qwr/giphy.gif"></img>
+                        </div>
+                    }
+                </>
+            )
+        }
+    } else {
         return (
-            <>
-                {myGames.length ?
-                    <div className='EventList-grid'>
-                        {myGames.map(event =>
-                            <EventListCard
-                                event={event}
-                                user={user}
-                                handleShow={handleShow}
-                                history={history}
-                            />
-                        )
-                        }
-                    </div>
-                    :
-                    <div>
-                        <img src="https://media.giphy.com/media/vZROLXfaqhbhHO8qwr/giphy.gif"></img>
-                    </div>
-                }
-            </>
-        )
-    }
-    else {
-        console.log("This is all events")
-        return (
-            <>
-                {events.length ?
-                    <div className='EventList-grid'>
-                        {events.map(event =>
-                            <EventListCard
-                                key={event._id}
-                                event={event}
-                                user={props.user}
-                                handleShow={handleShow}
-                                history={history}
-                            />
-                        )}
-                    </div>
-                    :
-                    <div>
-                        <img src="https://media.giphy.com/media/vZROLXfaqhbhHO8qwr/giphy.gif"></img>
-                    </div>
-                }
-            </>
+            <div>
+                <img src="https://media.giphy.com/media/vZROLXfaqhbhHO8qwr/giphy.gif"></img>
+            </div>
         )
     }
 }
