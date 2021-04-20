a8a87586c51a0d4cc967af7ae88e395ad52ceac0
davidisurf <davidisurf@hotmail.com>
2021-04-20 17:25:36 -0400

    Button hover not working

--- src/components/LocationDetails/LocationDetails.jsx
+++ src/components/LocationDetails/LocationDetails.jsx
@@ -13,38 +13,40 @@ function LocationDetails(props) {
 
   return (
     <>
-      <Card style={{ width: '18rem' }}>
-        {/*<Card.Img variant="top" src="holder.js/100px180"/>*/}
-        <Card.Body>
-          <Card.Title>{props.location.state.park.name}</Card.Title>
-          <img
-            src={props.location.state.park.icon}
-            className="park"
-            alt="hoop'n"
-          /><br />
-          <span>Google Rating: {props.location.state.park.rating}</span><br />
-          {/* <span>{props.weather.[{}]}</span><br />  */}
+      <div className='EventList-grid'>
+        <Card style={{ width: '18rem' }}>
+          {/*<Card.Img variant="top" src="holder.js/100px180"/>*/}
+          <Card.Body>
+            <Card.Title>{props.location.state.park.name}</Card.Title>
+            <img
+              src={props.location.state.park.icon}
+              className="park"
+              alt="hoop'n"
+            /><br />
+            <span>Google Rating: {props.location.state.park.rating}</span><br />
+            {/* <span>{props.weather.[{}]}</span><br />  */}
 
-          {/* <button onClick={gameHandler}>Details</button> */}
+            {/* <button onClick={gameHandler}>Details</button> */}
 
-          <Link className="button"
-            to={{
-              pathname: '/events/add',
-              props: { location }
-            }}
-          >Create Event</Link><br></br>
-          <Link className="button"
-            to={{
-              pathname: '/locations/review',
-              props: { location }
-            }}
-          >Add Review</Link><br></br>
-          
-        
-        {/* <Link className="button" onClick={reviewHandler}>Add Review</Link><br></br> */}
-        <h6>Display Reviews Here</h6>
-        </Card.Body>
-    </Card>
+            <Link className="button"
+              to={{
+                pathname: '/events/add',
+                props: { location }
+              }}
+            >Create Event</Link><br></br>
+            <Link className="button"
+              to={{
+                pathname: '/locations/review',
+                props: { location }
+              }}
+            >Add Review</Link><br></br>
+
+
+            {/* <Link className="button" onClick={reviewHandler}>Add Review</Link><br></br> */}
+            <h6>Display Reviews Here</h6>
+          </Card.Body>
+        </Card>
+      </div>
     </>
   )
 }
