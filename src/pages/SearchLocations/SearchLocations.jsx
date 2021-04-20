fab96002938e2b2fbda56fb44b58ca0e7c41f886
davidisurf <davidisurf@hotmail.com>
2021-04-20 19:49:58 -0400

    New layout locations pages

--- src/pages/SearchLocations/SearchLocations.jsx
+++ src/pages/SearchLocations/SearchLocations.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 import "./SearchLocations.css";
 import LocationDetails from "../../components/LocationDetails/LocationDetails";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Card from "react-bootstrap/Card"
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+
+
 function SearchLocations(props) {
-  const { event, user, places, weather,history } = props;
+  const { event, user, places, weather, history } = props;
   // const handler=(e)=>{console.log(e.id, '\n this is e');history.push({ pathname: '/location/details', props: {event}})}
   return (
     <>
       {places ? (
-        <div>
-          <span>Near by Parks</span><br />
+        <div className="locations-search" >
+          {/* <span>Near by Parks</span><br />
           {places.map((park) => (
-            <span>
-              <Link to={{pathname: '/location/details', state: {park}}}>
+            // <span>
+            <Link to={{ pathname: '/location/details', state: { park } }}>
               {park.name}
-              </Link>
-              <br></br>
-            </span>
-          ))}
+            </Link>
+            // <br></br>
+            // </span>
+          ))} */}
+
+          <Card style={{ width: 'auto', margin: '20px' }}>
+            <Card.Header style={{ fontSize: '25px', fontWeight: 'bolder' }}>Near by Parks</Card.Header>
+            <ListGroup variant="flush">
+            {places.map((park) => (
+              <ListGroup.Item><Link to={{ pathname: '/location/details', state: { park } }}>
+              {park.name}
+            </Link></ListGroup.Item>
+            ))}
+            </ListGroup>
+          </Card>
+
         </div>
       ) : (
         <div>
