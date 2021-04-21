45068dd69cb488e6a944ecd0515cd6feb249de23
mmpacker <mmpacker@gmail.com>
2021-04-21 11:51:09 -0400

    separate MyGames (/myEvents) to its own page

--- src/pages/MyEventsList/MyEventsList.jsx
+++ src/pages/MyEventsList/MyEventsList.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
 import './MyEventsList.css'
+import EventListCard from '../../components/EventListCard/EventListCard'
 
 function MyEventsList(props) {
+    const { user, events, handleShow, history } = props
+    const myGames = []
+
+    console.log("This is my events\n", user._id, "\n^^This is the user id\n", events)
+
+    events.forEach(ev => {
+        ev.participant.forEach(player => {
+            if (player._id === user._id) {
+                myGames.push(ev)
+            }
+
+        })
+    })
+
     return (
         <>
+            {myGames.length ?
+                <div className='EventList-grid'>
+                    {myGames.map(event =>
+                        <EventListCard
+                            event={event}
+                            user={user}
+                            handleShow={handleShow}
+                            history={history}
+                        />
+                    )
+                    }
+                </div>
+                :
+                <div>
+                    <img src="https://media.giphy.com/media/vZROLXfaqhbhHO8qwr/giphy.gif"></img>
+                </div>
+            }
         </>
     )
 }
